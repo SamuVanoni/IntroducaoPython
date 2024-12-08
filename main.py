020ae2308b5bbcbdf33013d3498546827a86b287
Samuel de Oliveira Vanoni <Samu.prog00@gmail.com>
2024-12-08 20:49:32 -0300

    :sparkles: feat: Adicionando menu do gerenciador de contatos e a opção de saída

---
+++ main.py
@@ -0,0 +1,16 @@
+while True:
+  print("\nMenu do Gerenciador de Contatos:")
+  print("1. Adicionar contato")
+  print("2. Ver contatos")
+  print("3. Atualizar contato")
+  print("4. Marcar/desmarcar contato como favorito")
+  print("5. Ver contatos marcados como favoritos")
+  print("6. Deletar contato")
+  print("7. Sair")
+
+  escolha = int(input("Digite a sua escolha: "))
+
+  if escolha == 7:
+    break
+
+print("Programa finalizado!")

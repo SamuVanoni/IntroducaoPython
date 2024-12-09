e5e8d0fbd23370263c143b7c749285f6ca43a394
Samuel de Oliveira Vanoni <Samu.prog00@gmail.com>
2024-12-08 21:01:13 -0300

    :sparkles: feat: Função 1 - adicionar contato

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+def adicionar_contato(contatos, nome, telefone, email):
+    contato = {"Nome": nome, "Telefone": telefone, "E-mail": email, "Favorito": False}
+    contatos.append(contato)
+    print(f"Contato {nome} foi adicionado(a) com sucesso!")
+    return
+
+contatos = []
 while True:
-  print("\nMenu do Gerenciador de Contatos:")
-  print("1. Adicionar contato")
-  print("2. Ver contatos")
-  print("3. Atualizar contato")
-  print("4. Marcar/desmarcar contato como favorito")
-  print("5. Ver contatos marcados como favoritos")
-  print("6. Deletar contato")
-  print("7. Sair")
+    print("\nMenu do Gerenciador de Contatos:")
+    print("1. Adicionar contato")
+    print("2. Ver contatos")
+    print("3. Atualizar contato")
+    print("4. Marcar/desmarcar contato como favorito")
+    print("5. Ver contatos marcados como favoritos")
+    print("6. Deletar contato")
+    print("7. Sair")
 
-  escolha = int(input("Digite a sua escolha: "))
+    escolha = int(input("Digite a sua escolha: "))
 
-  if escolha == 7:
-    break
+    if escolha == 1:
+        nome = input("Digite o nome do contato que deseja adicionar: ")
+        telefone = input("Digite o telefone do contato que deseja adicionar: ")
+        email = input("Digite o e-mail do contato que deseja adicionar: ")
+        adicionar_contato(contatos, nome, telefone, email)
+        print(contatos)
+
+    elif escolha == 7:
+        break
 
 print("Programa finalizado!")
